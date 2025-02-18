0dbc9187d3d7e892643d66bfb30238e8739e2625
Md-Raint-Rubbyat-Sultan <mdranitrubbyatsultan@gmail.com>
2025-02-18 22:00:42 +0600

    add circle animation

--- src/components/Home/Banner/BannerImage.jsx
+++ src/components/Home/Banner/BannerImage.jsx
@@ -3,12 +3,15 @@ import PropTypes from "prop-types";
 
 const BannerImage = ({ profile }) => {
   return (
-    <div className="w-fluid-img mx-auto rounded-full relative z-20">
-      <img
-        src={profile}
-        alt="profile"
-        className="w-full rounded-full object-contain"
-      />
+    <div className="flex items-center justify-center relative z-20">
+      <div className="w-fluid-img mx-auto border-8 rounded-full">
+        <img
+          src={profile}
+          alt="profile"
+          className="w-full rounded-full object-contain"
+        />
+      </div>
+      <div className="absolute w-fluid h-fluid bg-third rounded-full animate-circulate"></div>
     </div>
   );
 };
