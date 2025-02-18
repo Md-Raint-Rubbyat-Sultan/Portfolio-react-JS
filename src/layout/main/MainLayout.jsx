47ea6b6d688b3124484e2f57cb25c3eeccb18b5e
Md-Raint-Rubbyat-Sultan <mdranitrubbyatsultan@gmail.com>
2025-02-18 11:42:37 +0600

    add openning to mainlayout

--- src/layout/main/MainLayout.jsx
+++ src/layout/main/MainLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "../../components/shared/Navbar/Navbar";
 import Footer from "../../components/shared/Footer/Footer";
-import OpeningAnimation from "../../components/Home/OpeningAnimation/OpeningAnimation";
+import OpeningAnimation from "../../components/OpeningAnimation/OpeningAnimation";
 
 const MainLayout = () => {
   return (
