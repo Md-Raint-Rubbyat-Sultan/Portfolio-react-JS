4a185340172468b0c9a9c3092ab0e0a1986d8904
Md-Raint-Rubbyat-Sultan <mdranitrubbyatsultan@gmail.com>
2025-03-19 19:38:18 +0600

    added new feature to theme

--- src/Constants/Themes.js
+++ src/Constants/Themes.js
@@ -15,7 +15,12 @@ const Themes = [
     textClr: "rgb(170, 150, 218)",
   },
   {
-    name: "dark",
+    name: "olive-silor",
+    bgClr: "rgb(254, 250, 224)",
+    textClr: "rgb(98, 111, 71)",
+  },
+  {
+    name: "dark-sheet",
     bgClr: "rgb(29, 22, 22)",
     textClr: "rgb(238, 238, 238)",
   },
--- src/components/shared/Navbar/NavThemeButton/NavThemeButton.jsx
+++ src/components/shared/Navbar/NavThemeButton/NavThemeButton.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 
 import { ThemeProvider } from "../../../../contextApis/ThemeContext/ThemeContext";
 import Themes from "../../../../Constants/Themes";
+import toCapital from "../../../../Constants/toCapital";
+import { MdCancelPresentation } from "react-icons/md";
 
 const NavThemeButton = ({ toggle, setToggle }) => {
   // themes
@@ -15,23 +17,35 @@ const NavThemeButton = ({ toggle, setToggle }) => {
 
   return (
     <div
-      className={`absolute top-[100%]  bg-final p-fluid rounded border-2 border-prime grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-fluid z-40 ${
+      className={`absolute top-[100%]  bg-final p-fluid rounded border-2 border-prime ${
         toggle ? "left-[5%]" : "-left-[100%]"
-      } transition-all delay-300`}
+      } transition-all overflow-y-auto z-40`}
     >
-      {themes?.map((theme) => (
-        <button
-          key={theme?.name}
-          onClick={() => handelTheme(theme.name)}
-          style={{
-            color: `${theme.textClr}`,
-            backgroundColor: `${theme.bgClr}`,
-          }}
-          className="text-fluid-xs py-fluid-xs px-fluid border-2 border-prime rounded cursor-pointer"
-        >
-          {theme?.name}
-        </button>
-      ))}
+      {/* flex flex-row-reverse justify-start items-start gap-fluid  */}
+      <div className="max-h-80 relative">
+        <div className="flex justify-between items-center mb-fluid-m text-fluid-m">
+          <p className="text-third">Select Theme</p>
+          <MdCancelPresentation
+            onClick={() => setToggle((prev) => !prev)}
+            className="cursor-pointer"
+          />
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-fluid">
+          {themes?.map((theme) => (
+            <button
+              key={theme?.name}
+              onClick={() => handelTheme(theme.name)}
+              style={{
+                color: `${theme.textClr}`,
+                backgroundColor: `${theme.bgClr}`,
+              }}
+              className="text-fluid-xs py-fluid-xs px-fluid w-fi border-2 border-prime rounded cursor-pointer"
+            >
+              {theme?.name.split("-").map((nm) => toCapital(nm) + " ")}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
